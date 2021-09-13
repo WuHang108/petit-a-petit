c686a4af4bb1a0254eef3bd2ce9f568e7e5cf33b
WANG Shengdian <shengdian.wang@outlook.com>
2021-09-13 11:20:35 +0800

    add codility

---
+++ codility/longest_binary_gap/main.cc
@@ -0,0 +1,11 @@
+#include "basic_input.h"
+#include "solution.hpp"
+
+int main() {
+    int N;
+    while(scanf("%d", &N) != EOF) {
+	int ans = solution(N);
+	printf("%d\n", ans);
+    }
+    return 0;
+}
---
+++ codility/longest_binary_gap/solution.hpp
@@ -0,0 +1,28 @@
+#include "basic_input.h"
+
+int solution(int N) {
+    int ans = 0;
+    int state = 0;
+    int gap = 0;
+    while(N > 0) {
+	int bit = N & 1;
+	N >>= 1;
+	if(bit > 0) {
+	    if(state == 0) {
+		state = 1;
+	    } else if(state==2){
+		ans = std::max(ans, gap);
+		gap = 0;
+		state = 1;
+	    }
+	} else { // bit = 0
+	    if(state == 1) {
+		gap++;
+		state = 2;
+	    } else if(state == 2) {
+		gap++;
+	    }
+	}
+    }
+    return ans;
+}
